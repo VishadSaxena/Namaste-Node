03a62f236e69491cf7b0ed8da6b2ca588a2a47d6
Vishad Saxena <vishad1710@gmail.com>
2026-09-16 15:12:35 +0530

    Mongo Connection
    Mongo db connected

--- calculate/database.js
+++ calculate/database.js
@@ -1,1 +1,18 @@
-const URL = "mongodb+srv://coolprankster17_db_user:OTmjLSSdRysXXrY8@namastenode.nci8b0c.mongodb.net/"
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb+srv://coolprankster17_db_user:OTmjLSSdRysXXrY8@namastenode.nci8b0c.mongodb.net/";
+
+const client = new MongoClient(uri);
+
+async function main(){
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db("TestingData");
+    const collection = db.collection("User Information");
+
+    // CRUD operations can be performed here
+
+    return 'done.'
+}
+
+main().then(console.log).catch(console.error).finally(() => client.close());
